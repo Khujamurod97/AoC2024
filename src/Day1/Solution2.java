9208b94bc45566fb99725ade4e7429ff1391c06e
Khujamurod Murtozakulov <mr.khujamurod.97@mail.ru>
2024-12-01 10:15:19 +0500

    day1:

--- src/Day1/Solution2.java
+++ src/Day1/Solution2.java
@@ -1,26 +1,23 @@
 package Day1;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-public class Solution1 {
+public class Solution2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Long> a = new ArrayList<>();
-        List<Long> b = new ArrayList<>();
         Long res = 0L;
+        Map<Long, Long> count = new HashMap<>();
         while(true) {
             String line = scanner.nextLine();
             if (line.equals("")) break;
             String[] s = line.split("\\s+");
             a.add(Long.parseLong(s[0]));
-            b.add(Long.parseLong(s[1]));
+            count.put(Long.parseLong(s[1]), count.getOrDefault(Long.parseLong(s[1]), 0L) + 1);
         }
         a.sort(Long::compareTo);
-        b.sort(Long::compareTo);
         for(int i = 0; i < a.size(); i ++) {
-            res += Math.abs(a.get(i) - b.get(i));
+            res += a.get(i) * count.getOrDefault(a.get(i), 0L);
         }
         System.out.println(res);
 
