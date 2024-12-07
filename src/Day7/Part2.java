926a307bd8c03fb02bbdff999b9943d62ecceafe
Khujamurod Murtozakulov <mr.khujamurod.97@mail.ru>
2024-12-07 10:25:40 +0500

    day 7

---
+++ src/Day7/Part2.java
@@ -0,0 +1,54 @@
+package Day7;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class Part2 {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        List<String> lines = new ArrayList<>();
+        while(true) {
+            String line = scanner.nextLine();
+            if (line.trim().isEmpty()) break;
+            lines.add(line);
+        }
+
+        BigInteger res =BigInteger.ZERO;
+        for (String line : lines) {
+            String[] split = line.split("\\:");
+            BigInteger total = new BigInteger(split[0]);
+            BigInteger[] arr = Arrays.stream(split[1].trim().split(" ")).map(BigInteger::new).toArray(BigInteger[]::new);
+            Queue<BigInteger> q = new LinkedList<>();
+            q.add(arr[0]);
+            for (int i = 1; i < arr.length; i++) {
+                BigInteger item = arr[i];
+                int qs = q.size();
+                for(int j = 0; j < qs; j++) {
+                    BigInteger t = q.poll();
+                    q.add(t.add(item));
+                    q.add(t.multiply(item));
+                    q.add(new BigInteger(t.toString() + item.toString() ));
+                }
+
+            }
+            boolean ok = false;
+            while (!q.isEmpty()) {
+                BigInteger t = q.poll();
+                if (t.equals(total)) {
+                    ok = true;
+                    break;
+                }
+            }
+            if (ok) res = res.add(total);
+        }
+        System.out.println(res);
+
+    }
+
+
+
+
+}
